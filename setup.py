c91168e172311b7b2050ed9c78da3536fa80d469
Mike Jarrett <msjarrett@gmail.com>
2020-10-15 13:02:43 -0700

    updates setup.py to increment version and and find_packages function

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(name='bikedata',
-      version='0.1',
+      version='0.2',
       description='GBFS analytics utilities',
       author='Mike Jarrett',
       author_email='msjarrett@gmail.com',
       url='https://notes.mikejarrett.ca',
-      packages=['bikedata'],
+      packages=find_packages(),
      )
